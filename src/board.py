df15a41ad48d9bff879f98bff17750aae0bb0d7b
Jacob Fisher <jacob@jcbfshr.com>
2020-06-08 17:22:12 +0100

    occupied square rendering

--- src/board.py
+++ src/board.py
@@ -1,40 +1,66 @@
+import curses
+
 class Square:
     def __init__(self, blank=False, final_x=False, final_y=False, initial_x=False, intersection=False, centre_char=" "):
-        if blank:
-            if initial_x:
-                if final_x:
+        self.occupied = False
+        self.colour = None
+        self.blank = blank
+        self.final_x = final_x
+        self.final_y = final_y
+        self.initial_x = initial_x
+        self.intersection = intersection
+        self.centre_char = centre_char
+        if self.blank:
+            if self.initial_x:
+                if self.final_x:
                     self.lines = ["|        " for x in range(4)]
-                    if final_y:
+                    if self.final_y:
                         self.lines.insert(0,"+-------+")
                     elif intersection:
                         self.lines.insert(0,"+       ")
                 else:
                     self.lines = ["|       " for x in range(4)]
-                    if final_y:
+                    if self.final_y:
                         self.lines.insert(0,"+-------")
             else:
-                if final_x:
+                if self.final_x:
                     self.lines = ["         " for x in range(4)]
-                    if final_y:
+                    if self.final_y:
                         self.lines.insert(0,"+-------+")
                 else:
                     self.lines = ["        " for x in range(4)]
-                    if final_y:
+                    if self.final_y:
                         self.lines.insert(0,"+-------")
         else:
-            if final_x:
+            if self.final_x:
                 self.lines = ["+-------+",
                             "|       |",
-                            f"|   {centre_char}   |",
+                            f"|   {self.centre_char}   |",
                             "|       |",
                             ]
-                if final_y:
+                if self.final_y:
                     self.lines.append("+-------+")
             else:
                 self.lines = ["+-------",
                             "|       ",
-                            f"|   {centre_char}   ",
+                            f"|   {self.centre_char}   ",
                             "|       ",
                             ]
-                if final_y:
+                if self.final_y:
                     self.lines.append("+-------")
+    
+    def occupy(self, occupy=True):
+        if occupy:
+            self.occupied = True
+            if not self.blank:
+                if self.final_x:
+                    self.lines[2] = f"|  ({self.centre_char})  |"
+                else:
+                    self.lines[2] = f"|  ({self.centre_char})  "
+        else:
+            self.occupied = False
+            if not self.blank:
+                if self.final_x:
+                    self.lines[2] = f"|   {self.centre_char}  |"
+                else:
+                    self.lines[2] = f"|   {self.centre_char}   "
--- src/render.py
+++ src/render.py
@@ -1,46 +1,71 @@
 from time import sleep
 import curses
 import board
+import sys
 
-stdscr = curses.initscr()
-curses.start_color()
-board_squares = [[
-        board.Square(),
-        board.Square(centre_char="*"),
-        board.Square(blank=True,final_y=True),
-        board.Square(blank=True),
-        board.Square(),
-        board.Square(),
-        board.Square(),
-        board.Square(final_y=True,centre_char="*"),
-    ],
-    [
-        board.Square(),
-        board.Square(),
-        board.Square(),
-        board.Square(),
-        board.Square(centre_char="*"),
-        board.Square(),
-        board.Square(),
-        board.Square(final_y=True),
-    ],
-    [
-        board.Square(final_x=True),
-        board.Square(centre_char="*",final_x=True),
-        board.Square(blank=True,final_x=True,final_y=True,initial_x=True),
-        board.Square(blank=True,final_x=True,initial_x=True,intersection=True),
-        board.Square(final_x=True),
-        board.Square(final_x=True),
-        board.Square(final_x=True),
-        board.Square(centre_char="*",final_x=True,final_y=True),
-    ],
-]
+class Renderer:
+    def __init__(self):
+        self.board_squares = [[
+                board.Square(),
+                board.Square(centre_char="*"),
+                board.Square(blank=True,final_y=True),
+                board.Square(blank=True),
+                board.Square(),
+                board.Square(),
+                board.Square(),
+                board.Square(final_y=True,centre_char="*"),
+            ],
+            [
+                board.Square(),
+                board.Square(),
+                board.Square(),
+                board.Square(),
+                board.Square(centre_char="*"),
+                board.Square(),
+                board.Square(),
+                board.Square(final_y=True),
+            ],
+            [
+                board.Square(final_x=True),
+                board.Square(centre_char="*",final_x=True),
+                board.Square(blank=True,final_x=True,final_y=True,initial_x=True),
+                board.Square(blank=True,final_x=True,initial_x=True,intersection=True),
+                board.Square(final_x=True),
+                board.Square(final_x=True),
+                board.Square(final_x=True),
+                board.Square(centre_char="*",final_x=True,final_y=True),
+            ],
+        ]
 
-for column in range(len(board_squares)):
-    for row in range(len(board_squares[column])):
-        for y in range(len(board_squares[column][row].lines)):
-            stdscr.addstr((row*4) + y,column*8,board_squares[column][row].lines[y])
-            stdscr.refresh()
+    def start(self):
+        stdscr = curses.initscr()
+        stdscr.clear()
+        curses.def_shell_mode()
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+        return stdscr
+
+    def dump(self, stdscr):
+        for column in range(len(self.board_squares)):
+            for row in range(len(self.board_squares[column])):
+                for y in range(len(self.board_squares[column][row].lines)):
+                    square = self.board_squares[column][row]
+                    line = square.lines[y]
+                    try:
+                        if square.occupied and y == 2:
+                            stdscr.addstr((row*4) + y,column*8,line[0])
+                            stdscr.addstr((row*4) + y,column*8 + 1,line[1:])
+                            stdscr.addstr((row*4) + y,column*8 + 1,line[1:],curses.color_pair(1))
+                            stdscr.addstr((row*4) + y,column*8 + (len(line)-1),line[len(line)-1:])
+                        else:
+                            stdscr.addstr((row*4) + y,column*8,line)
+                    except curses.error as e:
+                        curses.intrflush(True)
+                        curses.reset_shell_mode()
+                        print(f"{e}: Your terminal can't render this ­— try maximising the window. We can't support monitors below 800x600")
+                        sys.exit(1)
+
+        stdscr.refresh()
 
 """
 +-------+-------+-------+
